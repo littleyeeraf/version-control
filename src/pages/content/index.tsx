5fad8418f8fc7b2665888035369d8082ff474e65
satang <117140785+littleyeeraf@users.noreply.github.com>
2023-04-17 16:45:33 +0700

    Add type

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function Content() {
+export default function Content(): JSX.Element {
   return (
     <>
       <Head>
--- src/pages/content/new.tsx
+++ src/pages/content/new.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-export default function NewContent() {
+export default function NewContent(): JSX.Element {
   return (
     <>
       <Head>
