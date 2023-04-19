a78b602a42ebc10d25cbd92c9c394cf1a52b0730
satang <117140785+littleyeeraf@users.noreply.github.com>
2023-04-19 10:57:13 +0700

    Remove db directory

--- src/db/connect.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const DB: Pool = new Pool({
-  user: "yeeraf",
-  password: "secret",
-  host: "localhost",
-  port: 5432,
-  database: "kontrol",
-});
--- src/db/sql/contents.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS contents (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL
-);
