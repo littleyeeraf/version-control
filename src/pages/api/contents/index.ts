917e4578fda3374ef345cf735a70eb6307d55371
satang <117140785+littleyeeraf@users.noreply.github.com>
2023-04-20 12:04:58 +0700

    Fix POST api

--- src/pages/api/contents/index.ts
+++ src/pages/api/contents/index.ts
@@ -2,8 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/lib/prisma";
 
-async function createContent(title: string, body: string) {
-  return await prisma.contents.create({ data: { title: title, body: body } });
+async function createContent(title: string, body: string, publishedAt: Date) {
+  return await prisma.contents.create({
+    data: { title: title, body: body, publishedAt: publishedAt },
+  });
 }
 
 async function getContent() {
@@ -19,8 +21,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       res.status(200).json(contents);
       break;
     case "POST":
-      const { title, body } = req.body;
-      const content = await createContent(title, body);
+      const { title, body, publishedAt } = req.body;
+      const content = await createContent(title, body, publishedAt);
       res.status(201).json(content);
       break;
     default:
